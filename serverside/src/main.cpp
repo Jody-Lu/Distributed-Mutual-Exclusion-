a3a784c14706534adbc77f46045f98bed1c98554
Jody-Lu <b00901192@ntu.edu.tw>
2017-02-14 13:01:51 -0600

    remove comment

--- serverside/src/main.cpp
+++ serverside/src/main.cpp
@@ -324,12 +324,10 @@ void *ProcessControlMessage(void *args)
 
 		printf( "Read %d Bytes\n", numBytesRead );
 
-		/*
 		if ( numBytesRead == 0 )
 		{
 			break;
 		}
-		*/
 
 		printf( "Read %d bytes, type: %s, from node: %d seqNo: %d\n", numBytesRead, m.type.c_str(), m.my_id, m.seqNo);
 
